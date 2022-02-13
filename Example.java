079451ee8b654817fc373d6864e882d925db1e9e
mike <michaelwilsonsj@hotmail.com>
2022-02-12 19:16:13 -0500

    init

---
+++ Example.java
@@ -0,0 +1,13 @@
+class Example {
+	public static void main(String[] args) {
+    double gallons;
+    double liters;
+
+    gallons = 1;
+
+    liters = gallons * 3.7854;
+
+    System.out.println(gallons + " gallons is " + liters + " liters.");
+      
+  }
+}
