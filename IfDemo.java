de6d2dccb5deccbdbc129b68ef6eef6e002886c6
mike <michaelwilsonsj@hotmail.com>
2022-02-12 19:45:31 -0500

    if statements

---
+++ IfDemo.java
@@ -0,0 +1,10 @@
+class IfDemo {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        a = 2;
+        b = 3;
+
+        if(a < b) System.out.println("a is less than b");
+    }
+}
